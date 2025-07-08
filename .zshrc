2fe1906ba5ceab869ac0d532336976aad97f16fc
nashyvan <yaroslavnashyvan@gmail.com>
2025-07-08 04:34:09 +0300

    chore: update configuration

--- .zshrc
+++ .zshrc
@@ -13,11 +13,8 @@ source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
 source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+# Set custom path for Zsh completion dump file, using hostname to avoid conflicts across machines
 export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
-#source $ZSH/oh-my-zsh.sh
-
-# SSH kitty fix
-alias s="kitty +kitten ssh"
 
 # Neovim
 alias n='nvim'
@@ -25,11 +22,10 @@ alias n='nvim'
 # Tmux
 alias tn='tmux new -s'
 
-# Ripgrep aliases
+# Ripgrep
 alias rgl='rg -l'
 
-
-# Git aliases
+# Git
 alias gs='git status --short'
 alias ga='git add'
 alias gb='git branch'
@@ -49,35 +45,41 @@ alias gda='git diff --color --name-only --diff-filter=A'
 alias gdm='git diff --color --name-only --diff-filter=M'
 alias gdd='git diff --color --name-only --diff-filter=D'
 
-
 # ls
 alias l="eza -lah --git"
 alias lt="eza -lahT --git"
 
-
 # cd
-#alias cdd='cd ~/Downloads'
 alias cdd='cd ~/Developer'
 
-
-# Docker aliases
+# Docker
 alias dps='docker ps'
 
-
-#TODO
+# TODO
 alias td="cat $NOTES_DIR/TODO.md"
 alias tde="(cd $NOTES_DIR && vim TODO.md && git add TODO.md && git commit -m 'update TODO')"
 
-
-#NOTES
+# Notes
 alias nt="(cd $NOTES_DIR && vim tmp.md)"
 alias nu="(cd $NOTES_DIR && git add . && git commit -m 'update notes')"
 
+# Set default editor to Neovim
 export EDITOR=nvim
+
+# Prepend /usr/local/sbin to the PATH
 export PATH="/usr/local/sbin:$PATH"
 
+# Load environment variables from .env file used by Borgmatic
 export $(cat ~/.config/borgmatic/.env | xargs)
 
+# Add Homebrew-managed zsh completions to function path
 fpath+=/opt/homebrew/share/zsh/site-functions
+
+# Initialize and set the "pure" prompt
 autoload -U promptinit && promptinit
 prompt pure
+
+# Set up Node Version Manager (NVM)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # Load NVM
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # Load NVM bash completion
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,6 +8,7 @@
 --- My Wezterm config file
 
 local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 local act = wezterm.action
 
 -- local fish_path = "/opt/homebrew/bin/fish"
@@ -19,21 +20,28 @@ if wezterm.config_builder then
 end
 
 -- Settings
--- config.default_prog = { fish_path, "-l" }
+config.default_prog = { "/bin/zsh" }
+config.term = "wezterm"
 
 config.color_scheme = "vscode-dark"
--- config.colors = {
---    background = '#1D1D1D'
--- }
+config.colors = {
+	background = '#1c1c1c',
+	tab_bar = {
+		active_tab = {
+			bg_color = '#1f1f1f',
+			fg_color = '#c0c0c0',
+		},
+	},
+}
 
 config.font = wezterm.font_with_fallback({
-	{ family = "JetbrainsMono Nerd Font", scale = 1.2, weight = "Bold" },
-	{ family = "Iosevka Nerd Font", scale = 1.2, weight = "Medium" },
+	{ family = "JetbrainsMono Nerd Font",  scale = 1.2, weight = "Bold" },
+	{ family = "Iosevka Nerd Font",        scale = 1.2, weight = "Medium" },
 	{ family = "FantasqueSansM Nerd Font", scale = 1.3 },
 })
-config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
-config.window_close_confirmation = "AlwaysPrompt"
+-- config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE" -- |TITLE|INTEGRATED_BUTTONS
+config.window_close_confirmation = "NeverPrompt"
 config.scrollback_lines = 5000
 config.default_workspace = "main"
 
@@ -46,21 +54,37 @@ config.inactive_pane_hsb = {
 -- Keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
+	{
+		key = "R",
+		mods = "LEADER|SHIFT",
+		-- action = act.ReloadConfiguration,
+		action = wezterm.action_callback(function(window, pane)
+			window:set_right_status("✅ Reloaded config!")
+			window:perform_action(wezterm.action.ReloadConfiguration, pane)
+		end),
+	},
+	{
+		key = "o",
+		mods = "CMD",
+		action = wezterm.action.EmitEvent("trigger-test-overlay"),
+	},
+	{ key = 'L',          mods = 'CTRL',        action = wezterm.action.ShowDebugOverlay },
+
 	-- Send C-a when pressing C-a twice
-	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
-	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+	{ key = "a",          mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c",          mods = "LEADER",      action = act.ActivateCopyMode },
+	{ key = "phys:Space", mods = "LEADER",      action = act.ActivateCommandPalette },
 
 	-- Pane keybindings
-	{ key = "s", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "v", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
-	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
-	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
-	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-	{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	{ key = "s",          mods = "LEADER",      action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "v",          mods = "LEADER",      action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h",          mods = "LEADER",      action = act.ActivatePaneDirection("Left") },
+	{ key = "j",          mods = "LEADER",      action = act.ActivatePaneDirection("Down") },
+	{ key = "k",          mods = "LEADER",      action = act.ActivatePaneDirection("Up") },
+	{ key = "l",          mods = "LEADER",      action = act.ActivatePaneDirection("Right") },
+	{ key = "q",          mods = "LEADER",      action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
+	{ key = "o",          mods = "LEADER",      action = act.RotatePanes("Clockwise") },
 	-- We can make separate keybindings for resizing panes
 	-- But Wezterm offers custom "mode" in the name of "KeyTable"
 	{
@@ -91,13 +115,79 @@ config.keys = {
 		}),
 	},
 	-- Key table for moving tabs around
-	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+	{ key = "m", mods = "LEADER",       action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
 	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
 	{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
 	{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 
+	{
+		key = "f",
+		mods = "CMD|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 	-- Lastly, workspace
 	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+	-- {
+	-- 	key = "s",
+	-- 	mods = "CMD",
+	-- 	action = wezterm.action_callback(function(win, pane)
+	-- 		resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+	-- 		resurrect.window_state.save_window_action()(win, pane)
+	-- 		win:set_right_status("✅ Reloaded config!")
+	-- 		win:perform_action(
+	-- 			wezterm.action.EmitEvent("save-session"),
+	-- 			pane
+	-- 		)
+	-- 	end),
+	{
+		key = "s",
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			-- First, show the status message
+			win:set_right_status("💾 Session saved!")
+
+			-- Clear the status message after 1.5 seconds
+			-- wezterm.time.call_after(1.5, function()
+			-- 	win:set_right_status("")
+			-- end)
+
+			-- Perform saving actions with a slight delay to let the UI update
+			wezterm.time.call_after(0.01, function()
+				resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+				resurrect.window_state.save_window_action()(win, pane)
+				win:perform_action(
+					wezterm.action.EmitEvent("save-session"),
+					pane
+				)
+			end)
+		end),
+	},
+	{
+		key = "r",
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)")
+				id = string.match(id, "([^/]+)$")
+				id = string.match(id, "(.+)%..+$")
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				if type == "workspace" then
+					local state = resurrect.state_manager.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.state_manager.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.state_manager.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end)
+		end),
+	},
 }
 -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
 for i = 1, 9 do
@@ -110,20 +200,20 @@ end
 
 config.key_tables = {
 	resize_pane = {
-		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "h",      action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j",      action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k",      action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l",      action = act.AdjustPaneSize({ "Right", 1 }) },
 		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
+		{ key = "Enter",  action = "PopKeyTable" },
 	},
 	move_tab = {
-		{ key = "h", action = act.MoveTabRelative(-1) },
-		{ key = "j", action = act.MoveTabRelative(-1) },
-		{ key = "k", action = act.MoveTabRelative(1) },
-		{ key = "l", action = act.MoveTabRelative(1) },
+		{ key = "h",      action = act.MoveTabRelative(-1) },
+		{ key = "j",      action = act.MoveTabRelative(-1) },
+		{ key = "k",      action = act.MoveTabRelative(1) },
+		{ key = "l",      action = act.MoveTabRelative(1) },
 		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
+		{ key = "Enter",  action = "PopKeyTable" },
 	},
 }
 
@@ -133,6 +223,14 @@ config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
 config.tab_max_width = 50
 config.tab_bar_at_bottom = false
+wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
+resurrect.state_manager.periodic_save({
+	interval_seconds = 300,
+	save_tabs = true,
+	save_windows = true,
+	save_workspaces = true,
+})
+resurrect.state_manager.set_max_nlines(5000)
 wezterm.on("update-status", function(window, pane)
 	-- Workspace name
 	local stat = window:active_workspace()
@@ -189,11 +287,11 @@ wezterm.on("update-status", function(window, pane)
 		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
 		{ Text = " | " },
-		{ Foreground = { Color = "#e0af68" } },
+		-- { Foreground = { Color = "#e0af68" } },
 		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-		"ResetAttributes",
-		{ Text = " | " },
-		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		-- "ResetAttributes",
+		-- { Text = " | " },
+		-- { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
 		{ Text = "  " },
 	}))
 end)
